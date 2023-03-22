e1f8fec9fe657d3f99216719dee9677cb63087e6
Cade Chaney <cadechaney@live.com>
2023-03-22 17:23:41 -0500

    Add tests for take turn in round file

--- src/Round.js
+++ src/Round.js
@@ -1,8 +1,39 @@
+const Turn = require('../src/Turn');
 
 class Round {
-  constructor() {
+  constructor(deck) {
+    this.deck = deck
+    this.turns = 0
+    this.incorrectGuesses = []
+    this.correctGuesses = []
+    
+  }
+
+  returnCurrentCard() {
+    return this.deck.cards[this.turns]
+  }
+
+  takeTurn(guess) {
+    let currentTurn = new Turn(guess, this.returnCurrentCard())
+    this.turns ++
+    if (guess !== currentTurn.card.correctAnswer) {
+      this.incorrectGuesses.push(currentTurn.card.id)
+    } 
+    else if (guess === currentTurn.card.correctAnswer) {
+      this.correctGuesses.push(currentTurn.card.id)
+    }
+    
+   return currentTurn.giveFeedback()
+  }
+
+  calculatePercentCorrect() {
+    return Math.floor((1 - (this.incorrectGuesses.length / this.turns)) * 100)
+  }
+
+  endRound() {
 
   }
+
 }
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -15,11 +15,12 @@ let deck;
 describe('Round', () => {
 
   beforeEach('setup', () => {
-    round = new Round(deck)
-    deck = new Deck([card1, card2, card3])
+    turn = new Turn()
     card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     card2 =  new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck = new Deck([card1, card2, card3])
+    round = new Round(deck)
   })
 
   it('should be a function', () => {
@@ -32,10 +33,46 @@ describe('Round', () => {
     expect(round).to.be.an.instanceOf(Round)
   })
 
-  it('currentCard should be a method to return the first card at the start of the round', () => {
+  it('should have a deck of cards', () => {
     
-    expect(round.currentCard()).to.equal(card1)
+    expect(round.deck).to.equal(deck)
   })
 
+  it('should have a method that returns current card', () => {
+    round.takeTurn()
+    
+    expect(round.returnCurrentCard(), card1)
+  })
+
+  it('the next card becomes the current card', () => {
+    round.takeTurn()
+    round.takeTurn()
+    expect(round.returnCurrentCard(), card2)
+  })
+
+  it('should be able to store correct Answers', () => {
+    expect(round.correctGuesses).to.deep.equal([])
+  })
+
+  it('should be able to store incorrect answers', () => {
+    expect(round.incorrectGuesses).to.deep.equal([])
+  })
+
+  it('should give feedback if answer is incorrect', () => {
+    expect(round.takeTurn('pug')).to.deep.equal('incorrect!')
+  })
+
+  it('should give feedback if answer is correct', () => {
+    expect(round.takeTurn('sea otter')).to.deep.equal('correct!')
+  })
+
+  it('should calculate the percentage of correct answers', () => {
+    round.takeTurn('sea otter')
+    expect(round.calculatePercentCorrect()).to.equal(100)
+    round.takeTurn('gall bladdersssss')
+    expect(round.calculatePercentCorrect()).to.equal(50)
+    round.takeTurn('Fitzgerald')
+    expect(round.calculatePercentCorrect()).to.equal(66)
+  })
 })
 
